31b42f2396e0eadac3eb9f0000209d23c939fe54
LuisFerLCC <luisfern837@gmail.com>
2025-06-09 17:45:31 -0600

    PUT y GET By ID implementados

--- Sources/MusicApi/routes.swift
+++ Sources/MusicApi/routes.swift
@@ -15,11 +15,34 @@ func routes(_ app: Application) throws {
         return albums
     }
 	
+    app.get("api", "albums", ":id") { req async throws -> Album in
+		guard let album = try await Album.find(req.parameters.get("id"), on: req.db) else {
+			throw Abort(.notFound, reason: "El registro no ha sido encontrado")
+		}
+		
+		return album
+    }
+	
 	app.post("api", "albums") { req async throws -> Album in
 		let album = try req.content.decode(Album.self)
 		try await album.save(on: req.db)
 		
 		return album
+	}
+	
+	app.put("api", "albums", ":id") { req async throws -> Album in
+		guard let existingAlbum = try await Album.find(req.parameters.get("id"), on: req.db) else {
+			throw Abort(.notFound, reason: "El registro no ha sido encontrado")
+		}
+		
+		let updatedAlbum = try req.content.decode(Album.self)
+		existingAlbum.title = updatedAlbum.title
+		existingAlbum.description = updatedAlbum.description
+		existingAlbum.artist = updatedAlbum.artist
+		existingAlbum.image = updatedAlbum.image
+		
+		try await existingAlbum.update(on: req.db)
+		return existingAlbum
 	}
 
     try app.register(collection: TodoController())
